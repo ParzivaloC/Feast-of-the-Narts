11882457540182f46ff2f67457bf21c81b831e8d
ParzivaloC <bicherahovartur12@mail.ru>
2026-04-29 22:10:07 +0300

    Добавьте файлы проекта.

---
+++ Feast of the Narts/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Feast_of_the_Narts.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Feast_of_the_Narts.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Feast of the Narts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Feast_of_the_Narts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
